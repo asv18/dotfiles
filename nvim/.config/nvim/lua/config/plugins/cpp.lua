611de6a250048b6ec78eecab5a65ff93eb9e50e1
Aarnav Srivastava <70161949+AarnavSrivastava@users.noreply.github.com>
2024-12-30 14:16:41 -0500

    null-ls issues - find alternative soon

--- nvim/.config/nvim/lua/config/plugins/cpp.lua
+++ nvim/.config/nvim/lua/config/plugins/cpp.lua
@@ -1,30 +1,30 @@
 return {
-	"jose-elias-alvarez/null-ls.nvim",
-	config = function()
-		local null = require("null-ls.nvim")
-		local augroup = vim.api.nvim_create_augroup("ClangLspFormatting", {})
-
-		null.setup({
-			sources = {
-				null.builtins.formatting.clang_format
-			},
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({
-						buffer = bufnr,
-						group = augroup,
-					})
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ bufnr = bufnr })
-						end,
-					})
-					-- for different styling format, run --style <style_name> --dump-config > .clang_format in mason/bin/clang-format
-				end
-			end,
-		})
-	end,
-	ft = { "cpp", "c" }
+	-- "jose-elias-alvarez/null-ls.nvim",
+	-- config = function()
+	-- 	local null = require("null-ls.nvim")
+	-- 	local augroup = vim.api.nvim_create_augroup("ClangLspFormatting", {})
+	--
+	-- 	null.setup({
+	-- 		sources = {
+	-- 			null.builtins.formatting.clang_format
+	-- 		},
+	-- 		on_attach = function(client, bufnr)
+	-- 			if client.supports_method("textDocument/formatting") then
+	-- 				vim.api.nvim_clear_autocmds({
+	-- 					buffer = bufnr,
+	-- 					group = augroup,
+	-- 				})
+	-- 				vim.api.nvim_create_autocmd("BufWritePre", {
+	-- 					group = augroup,
+	-- 					buffer = bufnr,
+	-- 					callback = function()
+	-- 						vim.lsp.buf.format({ bufnr = bufnr })
+	-- 					end,
+	-- 				})
+	-- 				-- for different styling format, run --style <style_name> --dump-config > .clang_format in mason/bin/clang-format
+	-- 			end
+	-- 		end,
+	-- 	})
+	-- end,
+	-- ft = { "cpp", "c" }
 }
