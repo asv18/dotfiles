2171ab526e29b40b827df0670a6d8271c3b174d8
Aarnav Srivastava <70161949+AarnavSrivastava@users.noreply.github.com>
2025-01-01 18:13:52 -0500

    nvim changes - guard works now

--- nvim/.config/nvim/lua/config/plugins/guard.lua
+++ nvim/.config/nvim/lua/config/plugins/guard.lua
@@ -3,16 +3,15 @@ return {
 	dependencies = "nvimdev/guard-collection",
 	event = "BufReadPre",
 	config = function()
-		local guard = require("guard")
 		local ft = require("guard.filetype")
 
 		ft("c,cpp,json"):fmt("clang-format")
 
 		--[[ local augroup = vim.api.nvim_create_augroup("ClangLspFormatting", {}) ]]
 
-		guard.setup({
+		vim.g.guard_config = {
 			fmt_on_save = true,
 			lsp_as_default_formatter = false,
-		})
+		}
 	end,
 }
